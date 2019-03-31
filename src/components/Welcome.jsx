40d23ec2a60b01d0f32c9ebfbdc6a12cf53f3336
Mark Sun <sunmark14@gmail.com>
2019-03-30 21:03:41 -0400

    Added Welcome page. Added material-ui

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+});
+
+function Welcome(props) {
+  const { classes } = props
+  return (
+    <div className={classes.root}>
+      <Typography variant="h3" gutterBottom>
+        Welcome to NC Food Bank
+      </Typography>
+      <Button variant="contained" color="primary" className={classes.button}>
+        Check-In
+      </Button>
+      <Button variant="contained" color="secondary" className={classes.button}>
+        Check-Out
+      </Button>
+    </div>
+  );
+}
+
+Welcome.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Welcome);
